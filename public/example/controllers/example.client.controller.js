3721495141c3af01c0808680c03868020f9954b9
Tucker <borisblade17@gmail.com>
2017-07-08 15:49:24 -0500

    Create an angular controller for example.

---
+++ public/example/controllers/example.client.controller.js
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+angular.module('example').controller('ExampleController', ['$scope', 
+    function($scope) {
+        $scope.name = 'MEAN Application';
+    }
+]);
