eedd6ad8e1439606c00e2ee78ed1c6c1b8f16cdb
Tucker <borisblade17@gmail.com>
2017-07-08 15:08:04 -0500

    Set up AngularJS and add two text boxes

---
+++ public/application.js
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var mainApplicationModuleName = 'mean';
+
+var mainApplicationModule = angular.module(mainApplicationModuleName, []);
+
+angular.element(document).ready(function() {
+    angular.bootstrap(document, [mainApplicationModuleName]);
+});
